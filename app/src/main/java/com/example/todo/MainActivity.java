05ba7f2aa73806f1cb3017530f166de5310960a7
dnikki <dnikita3101@gmail.com>
2021-05-26 19:04:39 +0530

    login condition applied.

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -30,7 +32,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Intent intent=new Intent(this,Profile.class);
-        startActivity(intent);
+        String fname=fullname.getText().toString();
+        String uname=username.getText().toString();
+
+        if(TextUtils.isEmpty(fname) || TextUtils.isEmpty(uname)){
+            Toast.makeText(this,"Fullname and Username must be filled.",Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Intent intent=new Intent(this,Profile.class);
+            intent.putExtra("fullname",fullname.getText().toString());
+            startActivity(intent);
+
+        }
     }
 }
--- app/src/main/java/com/example/todo/Profile.java
+++ app/src/main/java/com/example/todo/Profile.java
@@ -2,6 +2,7 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        String name= getIntent().getStringExtra("fullname");
+        getSupportActionBar().setTitle(name);
     }
 }
