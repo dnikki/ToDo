9c15fdb8596f3801fce66c27078b4f90d54538fe
dnikki <dnikita3101@gmail.com>
2021-05-26 19:23:32 +0530

    add button added.

--- app/src/main/java/com/example/todo/Profile.java
+++ app/src/main/java/com/example/todo/Profile.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -14,5 +15,10 @@ public class Profile extends AppCompatActivity {
 
         String name= getIntent().getStringExtra("fullname");
         getSupportActionBar().setTitle(name);
+    }
+
+    public void add(View view) {
+
+
     }
 }
