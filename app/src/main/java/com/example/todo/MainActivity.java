f3181d8b8e06e5768d7bc16e20e7584c92a6b942
dnikki <dnikita3101@gmail.com>
2021-05-26 18:38:57 +0530

    Login activity updated

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    EditText fullname,username;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+    }
+
+    private void init(){
+        fullname=findViewById(R.id.fullname);
+        username=findViewById(R.id.username);
+        login=findViewById(R.id.submit);
+    }
+
+    public void login(View view) {
+        Intent intent=new Intent(this,Profile.class);
+        startActivity(intent);
     }
 }
