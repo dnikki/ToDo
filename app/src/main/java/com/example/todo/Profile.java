ab0f1c284fe6ec1bfbef1ae8a1797db797478382
dnikki <dnikita3101@gmail.com>
2021-05-28 19:11:01 +0530

    dialog box fixed

--- app/src/main/java/com/example/todo/Profile.java
+++ app/src/main/java/com/example/todo/Profile.java
@@ -8,15 +8,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Profile extends AppCompatActivity {
+    TextView title,description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        title=(TextView)findViewById(R.id.title);
+        description=(TextView)findViewById(R.id.description);
 
         String name= getIntent().getStringExtra("fullname");
         getSupportActionBar().setTitle(name);
@@ -24,26 +29,35 @@ public class Profile extends AppCompatActivity {
 
     public void add(View view) {
 
-        Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show();
-        /*View view2= LayoutInflater.from(Profile.this).inflate(R.layout.add_notes_layout_dialog,null);
-        EditText title=view2.findViewById(R.id.texttitle);
-        EditText description=view2.findViewById(R.id.textdescription);
-        EditText submit=view2.findViewById(R.id.submit);
+       // Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show();
+        View view2= LayoutInflater.from(Profile.this).inflate(R.layout.add_notes_layout_dialog,null);
+        EditText titled=view2.findViewById(R.id.texttitle);
+        EditText descriptiond=view2.findViewById(R.id.textdescription);
+        Button submit=view2.findViewById(R.id.submit);
+
+
+
+
 
         AlertDialog.Builder alertDialog= new AlertDialog.Builder(this);
-        alertDialog.setView(view2);
-        alertDialog.setCancelable(false);
-        alertDialog.create();
-        alertDialog.show();
+        //alertDialog.setTitle("ADD NOTES");
+       alertDialog.setView(view2);
+       alertDialog.setCancelable(false);
+       AlertDialog dialog = alertDialog.create();
+       dialog.show();
+      // alertDialog.create();
+      // alertDialog.show();
 
 
-        submit.setOnClickListener(new View.OnClickListener() {
+       submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                alertDialog.setCancelable(true);
+                title.setText(titled.getText().toString());
+                description.setText(descriptiond.getText().toString());
+                dialog.hide();
 
             }
-        });*/
+        });
 
 
 
